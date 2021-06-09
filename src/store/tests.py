f7d49bff87ddec5674216e25616d39067e0e6425
zixxa <dezzinara@gmail.com>
2021-06-10 01:49:32 +0300

    make items test

--- src/store/tests.py
+++ src/store/tests.py
@@ -1,16 +1,25 @@
 from django.test import TestCase
-from store.models import Item, Type
+from src.store.models import Item, Type
 
 class ItemsTestCase(TestCase):
     def setUp(self):
         Type.objects.create(name='Мебель')
-        Item.objects.create(title='табуретка', type=Type.objects.get(name='Мебель'))
-        Item.objects.create(title='шкаф', type=Type.objects.get(name='Мебель'))
-        Item.objects.create(title='стол', type=Type.objects.get(name='Мебель'))
+        Type.objects.create(name='Шкафы', parent=Type.objects.get(name='Мебель'))
+        Type.objects.create(name='Стулья', parent=Type.objects.get(name='Мебель'))
+        Item.objects.create(title='Альфа', itemtype=Type.objects.get(name='Стулья'))
+        Item.objects.create(title='Бета', itemtype=Type.objects.get(name='Шкафы'))
+
+    def test_types(self):
+        type1 = Type.objects.get(name='Шкафы')
+        type2 = Type.objects.get(name='Стулья')
+        parent = Type.objects.get(name='Мебель')
 
     def test_items(self):
-        shelf = Item.objects.get(title='табуретка')
-        table = Item.objects.get(title='стол')
-        closet = Item.objects.get(title='шкаф')
-        self.assertEqual(table.slug(), 'stol')
-        self.assertEqual(closet.slug(), 'shkaf')
+        chair = Item.objects.get(title='Альфа', itemtype=Type.objects.get(name='Стулья'))
+        shelf = Item.objects.get(title='Бета', itemtype=Type.objects.get(name='Шкафы'))
+
+        self.assertEqual(chair.__str__(), 'Альфа')
+        self.assertTrue(chair.get_articel())
+
+        self.assertEqual(shelf.__str__(), 'Бета')
+        self.assertTrue(shelf.get_articel())

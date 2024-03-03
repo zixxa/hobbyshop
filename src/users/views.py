94a7d5b11ab4b6c983b3a2467d0d63e26f0f76be
zixxa <dezzinara@gmail.com>
2024-03-03 12:47:42 +0300

    SignUp view refactoring

--- src/users/views.py
+++ src/users/views.py
@@ -1,4 +1,6 @@
+from django.contrib import messages
 from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
@@ -9,11 +11,16 @@ from .forms import UserRegisterForm
 from django.views.generic.edit import CreateView
 
 
-class SignUpView(SuccessMessageMixin, CreateView):
+class SignUpView(SuccessMessageMixin, CreateView,LoginRequiredMixin):
     template_name = 'users/signup.html'
     success_url = reverse_lazy('login')
     form_class = UserRegisterForm
     success_message = "Your profile was created successfully"
+    error_message = 'Error in password or login (password must has at least 8 digits and symbols)'
+
+    def form_invalid(self, form):
+        messages.error(self.request, *form.errors.values())
+        return super().form_invalid(form)
 
 
 def logout(request):
